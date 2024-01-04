8ca2267d398ea480e885327be00d8c2a145d1933
Shreyas A S <137637016+shreyas-a-s@users.noreply.github.com>
2024-01-04 23:39:08 +0530

    Hide output of printf

--- install.sh
+++ install.sh
@@ -31,7 +31,7 @@ fi
 printf "\
 \nif [ -z "\$DISPLAY" ] && [ \$(tty) = /dev/tty1 ]; then\
 \n  startx\
-\nfi" | sudo tee -a /etc/zsh/zprofile
+\nfi" | sudo tee -a /etc/zsh/zprofile > /dev/null
 
 [ -d ~/.icewm ] || mkdir ~/.icewm
 cp .icewm/keys ~/.icewm/
