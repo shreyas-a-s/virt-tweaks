b753483a98d391af4471e6f60e21ca2da95bc7ef
Shreyas A S <137637016+shreyas-a-s@users.noreply.github.com>
2024-01-04 20:14:44 +0530

    Initial commit

---
+++ install.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+sudo apt-get install -y icewm neovim htop git pcmanfm nitrogen gnome-backgrounds xinit xterm lxterminal x11-xserver-utils command-not-found spice-vdagent xdg-utils dmenu xsel firefox-esr
+
+sudo update-alternatives --config x-terminal-emulator
+
