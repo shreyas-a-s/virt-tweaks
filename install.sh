54dec6945ea279c891273acfa0fee9611b1919e5
Shreyas A S <137637016+shreyas-a-s@users.noreply.github.com>
2024-01-04 21:21:06 +0530

    Add autologin to tty for systemd distros

--- install.sh
+++ install.sh
@@ -9,6 +9,12 @@ if [ -f /etc/inittab ]; then # For non-systemd distros
   sudo cp /etc/inittab /etc/inittab.bak
   sed -i '/1:2345/s/^/#/' /etc/inittab
   sed -i "/1:2345/a\1:2345:respawn:/bin/login -f $USER tty1 </dev/tty1 >/dev/tty1 2>&1" /etc/inittab
+else # For systemd distros
+  [ -d /etc/systemd/system/getty@tty1.service.d ] || sudo mkdir /etc/systemd/system/getty@tty1.service.d
+  printf "[Service]\
+  \nExecStart=\
+  \nExecStart=-/sbin/agetty -o '-p -f -- \\\\\\\\u' --noclear --autologin $USER %%I \$TERM\
+  \n" | sudo tee /etc/systemd/system/getty@tty1.service.d/autologin.conf > /dev/null
 fi
 
 # Enable automatic startx
