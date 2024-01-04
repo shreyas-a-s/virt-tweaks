e734fc87171db64e2fc6f83449dc28e936f0585d
Shreyas A S <137637016+shreyas-a-s@users.noreply.github.com>
2024-01-05 00:12:03 +0530

    Add wget

--- install.sh
+++ install.sh
@@ -3,7 +3,7 @@
 # Change directory
 SCRIPT_DIR=$(dirname -- "$( readlink -f -- "$0"; )") && cd "$SCRIPT_DIR" || exit
 
-sudo apt-get install -y curl htop git pcmanfm xinit xterm xfce4-terminal x11-xserver-utils command-not-found spice-vdagent dmenu xsel firefox-esr zsh zsh-autosuggestions zsh-syntax-highlighting icewm lxsession-logout
+sudo apt-get install -y curl wget htop git pcmanfm xinit xterm xfce4-terminal x11-xserver-utils command-not-found spice-vdagent dmenu xsel firefox-esr zsh zsh-autosuggestions zsh-syntax-highlighting icewm lxsession-logout
 
 wget https://github.com/neovim/neovim/releases/latest/download/nvim.appimage
 chmod +x nvim.appimage
