883c008ef2023867f60e9350ed69e51cc9b1f25b
Shreyas A S <137637016+shreyas-a-s@users.noreply.github.com>
2024-01-05 21:17:01 +0530

    Add 'execution of setup-auto-startx.sh' to install.sh

--- install.sh
+++ install.sh
@@ -7,6 +7,7 @@ SCRIPT_DIR=$(dirname -- "$( readlink -f -- "$0"; )") && cd "$SCRIPT_DIR" || exit
 ./scripts/install-programs.sh
 ./scripts/install-nvim.sh
 ./scripts/setup-autologin-to-tty.sh
+./scripts/setup-auto-startx.sh
 
 printf "### SET XDG DIR FOR ZSH ###\nZDOTDIR=~/.config/zsh\n" | sudo tee -a /etc/zsh/zshenv > /dev/null # set dotfile directory for zsh
 while ! chsh -s "$(command -v zsh)"; do :; done
