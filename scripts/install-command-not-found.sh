1b61fb44b971c20b3ba1024ec69667586040e3db
Shreyas A S <137637016+shreyas-a-s@users.noreply.github.com>
2024-01-05 21:25:02 +0530

    Add command-not-found handler and grub timeout set back

---
+++ scripts/install-command-not-found.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+if command -v apt-get > /dev/null; then # Install for debian-based distros
+  # Install the app
+  sudo apt-get install -y command-not-found
+
+  # Update database of command-not-found
+  sudo update-command-not-found
+  sudo apt-file update
+fi
+
+if command -v pacman > /dev/null; then # Install for archlinux-based distros
+  # Install the app
+  sudo pacman -S --noconfirm pkgfile
+
+  # Update database of pkgfile
+  sudo pkgfile -u
+fi
+
