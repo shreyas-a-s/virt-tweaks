a2e652732c1a175536973b7750ebe5952ca7462f
Shreyas A S <137637016+shreyas-a-s@users.noreply.github.com>
2024-01-05 23:27:26 +0530

    Reorder script executions alphabetically

--- install.sh
+++ install.sh
@@ -4,12 +4,12 @@
 SCRIPT_DIR=$(dirname -- "$( readlink -f -- "$0"; )") && cd "$SCRIPT_DIR" || exit
 
 # Custom scripts
-./scripts/install-programs.sh           # As the name suggests
-./scripts/install-nvim.sh               # Also as the name suggests. nvim i.e. neovim is a text-editor btw.
-./scripts/setup-autologin-to-tty.sh     # Autologin user to tty
-./scripts/setup-auto-startx.sh          # Whenever user logs into tty, startx is run launching default window manager
-./scripts/setup-bootloader.sh           # Set bootloader (grub/systemd-boot) timeout to 0 sec
 ./scripts/install-command-not-found.sh  # Install command-not-found handler
+./scripts/install-nvim.sh               # As the name suggests. nvim i.e. neovim is a text-editor btw.
+./scripts/install-programs.sh           # Also, as the name suggests
+./scripts/setup-auto-startx.sh          # Whenever user logs into tty, startx is run launching default window manager
+./scripts/setup-autologin-to-tty.sh     # Autologin user to tty
+./scripts/setup-bootloader.sh           # Set bootloader (grub/systemd-boot) timeout to 0 sec
 
 # Setup zsh as user shell
 printf "### SET XDG DIR FOR ZSH ###\nZDOTDIR=~/.config/zsh\n" | sudo tee -a /etc/zsh/zshenv > /dev/null # set dotfile directory for zsh
