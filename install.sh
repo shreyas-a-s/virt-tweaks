86104a262ba7a2c92f029066911d3af9fcd81e37
Shreyas A S <137637016+shreyas-a-s@users.noreply.github.com>
2024-01-05 23:25:45 +0530

    Add command to execute newly added scripts

--- install.sh
+++ install.sh
@@ -4,10 +4,12 @@
 SCRIPT_DIR=$(dirname -- "$( readlink -f -- "$0"; )") && cd "$SCRIPT_DIR" || exit
 
 # Custom scripts
-./scripts/install-programs.sh        # As the name suggests
-./scripts/install-nvim.sh            # Also as the name suggests. nvim i.e. neovim is a text-editor btw.
-./scripts/setup-autologin-to-tty.sh  # Autologin user to tty
-./scripts/setup-auto-startx.sh       # Whenever user logs into tty, startx is run launching default window manager
+./scripts/install-programs.sh           # As the name suggests
+./scripts/install-nvim.sh               # Also as the name suggests. nvim i.e. neovim is a text-editor btw.
+./scripts/setup-autologin-to-tty.sh     # Autologin user to tty
+./scripts/setup-auto-startx.sh          # Whenever user logs into tty, startx is run launching default window manager
+./scripts/setup-bootloader.sh           # Set bootloader (grub/systemd-boot) timeout to 0 sec
+./scripts/install-command-not-found.sh  # Install command-not-found handler
 
 # Setup zsh as user shell
 printf "### SET XDG DIR FOR ZSH ###\nZDOTDIR=~/.config/zsh\n" | sudo tee -a /etc/zsh/zshenv > /dev/null # set dotfile directory for zsh
