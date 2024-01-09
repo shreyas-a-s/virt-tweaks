b752ff0d6321ec23df3beb6ef677c2f7978c4a64
Shreyas A S <137637016+shreyas-a-s@users.noreply.github.com>
2024-01-09 17:06:43 +0530

    Add wget install to install-nvim.sh coz it's a dependency

--- scripts/install-nvim.sh
+++ scripts/install-nvim.sh
@@ -1,5 +1,10 @@
 #!/usr/bin/env bash
 
+# Install dependencies
+if command -v apt-get > /dev/null; then # Install for debian-based distros
+  sudo apt-get install -y wget
+fi
+
 wget https://github.com/neovim/neovim/releases/latest/download/nvim.appimage
 chmod +x nvim.appimage
 sudo mv nvim.appimage /usr/local/bin/nvim
