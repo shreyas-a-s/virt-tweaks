ec2da90178c9450a163a29c2bcb1495a9c473e66
Shreyas A S <137637016+shreyas-a-s@users.noreply.github.com>
2024-02-29 19:01:17 +0530

    Add fd-find version check and installation to install-nvim.sh

--- scripts/install-nvim.sh
+++ scripts/install-nvim.sh
@@ -5,6 +5,20 @@ if command -v apt-get > /dev/null; then # Install for debian-based distros
   sudo apt-get install -y gcc ripgrep wget xsel
 fi
 
+# Install fd
+FD_VERSION=apt-cache show fd-find | awk 'NR==3{print $2}'
+FD_MAJOR_VERSION="$(echo "$FD_VERSION" | awk -F . '{print $1}')"
+FD_MINOR_VERSION="$(echo "$FD_VERSION" | awk -F . '{print $2}')"
+if [ "$FD_MAJOR_VERSION" -gt 8 ]; then
+  sudo apt-get install -y fd-find
+elif [ "$FD_MAJOR_VERSION" -eq 8 ] && [ "$FD_MINOR_VERSION" -gt 3 ]; then
+  sudo apt-get install -y fd-find
+else
+  wget https://github.com/sharkdp/fd/releases/download/v8.3.2/fd_8.3.2_amd64.deb
+  sudo apt-get install ./fd_8.3.2_amd64.deb
+fi
+
+# Install neovim
 wget https://github.com/neovim/neovim/releases/latest/download/nvim.appimage
 chmod +x nvim.appimage
 sudo mv nvim.appimage /usr/local/bin/nvim
