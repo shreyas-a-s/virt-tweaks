9cafbecb6642b822b861935955740396afa35a27
Shreyas A S <137637016+shreyas-a-s@users.noreply.github.com>
2024-03-31 16:37:28 +0530

    feat(install-nvim.sh): Add zoxide

--- scripts/install-nvim.sh
+++ scripts/install-nvim.sh
@@ -2,7 +2,7 @@
 
 # Install dependencies
 if command -v apt-get > /dev/null; then # Install for debian-based distros
-  sudo apt-get install -y gcc ripgrep wget xsel fuse npm unzip python3-venv
+  sudo apt-get install -y gcc ripgrep wget xsel fuse npm unzip python3-venv zoxide
 fi
 
 # Install fd
